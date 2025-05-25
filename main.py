24dda2b04ae241271c6f33eaa1e12c26e7810a51
z-zeek <167027652+z-zeek@users.noreply.github.com>
2025-05-25 20:03:26 +0200

    add class definitions, methods and working jegy_foglalas()

--- main.py
+++ main.py
@@ -8,97 +8,161 @@ import os
 ## LégiTársaság: Tartalmazza a járatokat és saját attribútumot, mint például a légitársaság neve.
 ## JegyFoglalás: A járatok foglalásához szükséges osztály, amely egy utazásra szóló jegy foglalását tárolja.
 
+#Osztaly: jarat()
+#
+#vars: jaratszam 
+#- minden repter kap egy jaratszamot ami egy 3 jegyu szam,
+# maga a jarat a kindulo illetve a celallomas szamjegyeinek osszeadasa alkot egy jaratot
+# pl: Eger -> Vienna = BEL-1234 could also be for egKUL1725
+#
+#erkezo_repter
+#
+#indulo_repter
+#- 
+#
+#DEFAULT jegyar (?)
+#
+#
+
+class Jarat():
+    #Ha az index paros akkor belfoldi, ha paratlan akkor kulfoldi
+    kulfoldi_repterek = {"Vienna" : "VIE" , "Barcelona": "BCN", "Stockholm": "ARN"}
+    belfoldi_repterek = {"Budapest": "BUD", "Debrecen": "DEB", "Miskolc": "MCQ"}
+
+    def eval(kulfoldi_repterek, belfoldi_repterek, indulo_repter, erkezo_repter):
+
+        if(indulo_repter in belfoldi_repterek and erkezo_repter in belfoldi_repterek):
+            tipus = "BEL"
+            return tipus
+        else:
+            tipus = "KUL"
+            return tipus
+
+
+        #repter_lista = list(repterek.values())
+        
+    #class belfoldi_jarat: 
+class Belfoldi_jarat(Jarat):
+
+    def belfoldi_jaratszam(tipus, indulo_repter, erkezo_repter):
+
+        azonositok = {"Vienna" : 17, "Budapest" : 25, "Barcelona" : 31, "Debrecen" : 46, "Stockholm" : 59, "Miskolc" : 63}
+
+
+        jaratszam = tipus + str(azonositok[indulo_repter]) + str(azonositok[erkezo_repter])
+
+        return jaratszam
+
+    #class kulfoldi_jarat: 
+class Kulfoldi_jarat(Jarat):
+        
+    def kulfoldi_jaratszam(tipus, indulo_repter, erkezo_repter):
+
+        azonositok = {"Vienna" : 17, "Budapest" : 25, "Barcelona" : 31, "Debrecen" : 46, "Stockholm" : 59, "Miskolc" : 63}
+
+        jaratszam = tipus + azonositok[indulo_repter] + azonositok[erkezo_repter]
+
+        return jaratszam #:)
+
+    #class legi_tarsasag:
+class Legi_tarsasag(Belfoldi_jarat, Kulfoldi_jarat):
+        # azonosito = 
+        # jaratok = []
+        # legitarsasagok = {}
+
+    def calculator(tipus, legitarsasag):
+        legitarsasagok_belfold = {"RyanAir" : 0.78, "GDAir" : 0.8, "WizzAir" : 0.9}
+        legitarsasagok_kulfold = {"RyanAir" : 1.1, "GDAir" : 1.115, "WizzAir" : 1.12}
+
+        if(tipus == "BEL"):
+            if(legitarsasag in legitarsasagok_belfold):
+                ar = 40000
+                ar = ar * legitarsasagok_belfold[legitarsasag]
+
+                return ar
+            else: print("vele nem tudsz levegozni a levegoben")
+        else: 
+            print("add meg kivel akarsz a levegoben levegozni")
+
+            if(legitarsasag in legitarsasagok_kulfold):
+                ar = 40000
+                ar = ar * legitarsasagok_kulfold[legitarsasag]
+
+                return ar
+            else: print("vele nem tudsz levegozni a levegoben")
+
+
+    #class jegy_foglalas:
+class Jegy_foglalas(Jarat):
+
+    def jegy_foglalas(self):
+        
+        indulo_repter = input()
+        erkezo_repter = input()
+
+        tipus = Jarat.eval(Jarat.kulfoldi_repterek, Jarat.belfoldi_repterek, indulo_repter, erkezo_repter)
+
+        if(tipus == "BEL"):
+            jaratszam = Belfoldi_jarat.belfoldi_jaratszam(tipus, indulo_repter, erkezo_repter)
+        else:
+            jaratszam = Kulfoldi_jarat.kulfoldi_jaratszam(tipus, indulo_repter, erkezo_repter)
+        
+        print("add meg kivel akarsz a levegoben levegozni")
+        legitarsasag = input()
+
+
+        ar = Legi_tarsasag.calculator(tipus, legitarsasag)
+
+        print(indulo_repter + " " + erkezo_repter + " " + jaratszam + " " + str(ar))
+
+        
+
+
+    #def jegy_foglalas_lemondas(azonosito):
+
+
+    #def jegy_foglalasok_listazasa(foglalas):
+
+
+## Felhasználói interfész
+
+## Egyszerű felhasználói interfész, amely opciokent lehetővé teszi a következő műveleteket:
+## Foglalás lemondása
+## Jegy foglalása
+## Foglalások listázása
+
+## Funkciók
+
+## Jegy foglalása: A járatokra jegyet lehet foglalni, és visszaadja a foglalás árát.
+## Foglalás lemondása: A felhasználó lemondhatja a meglévő foglalást.
+## Foglalások listázása: Az összes aktuális foglalás listázása.
+
 class main_menu():
 
-    def init_foglalas():
-        #does txt exist?
-        #if no:
-        #   - create and populate sample jaratok
-        #
-        #if yes:
-        #   - load it in
-
     def program(option):
+
         if option == '1':
             ## Foglalasi funkcio
-            Jegyfoglalas.jegy_foglalas
+            foglalas = Jegy_foglalas()
+            foglalas.jegy_foglalas()   
         elif option == '2':
             ## Lemondasi funkcio
+            print("meg olyan nincse")
         elif option == '3':
             ## Listazasi funkcio 
+            print("meg olyan nincse")
         elif option == '9':
             print("A program bezárása...")
             exit()
         else:
             print("Helytelen menüpont, fussunk neki megegyszer!")
-        input("\nNyomjon Entert a folytatáshoz...")
+            input("\nNyomjon Entert a folytatáshoz...")
 
     def clear_screen():
         if os.name == 'nt':
             os.system('cls')
         else:
             os.system('clear')
-
-
-#Osztaly: jarat()
-#
-#vars: jaratszam 
-#- minden repter kap egy jaratszamot ami egy 3 jegyu szam,
-# maga a jarat a kindulo illetve a celallomas szamjegyeinek osszeadasa alkot egy jaratot
-# pl: Eger -> Vienna = 178250 (maybe 178-250), could also be startAirportLetters1725
-#
-#celallomas
-#- 
-#
-#jegyar
-#
-#
-
-    class Jarat():
-        azonositok = [17, 25, 31, 46, 59, 63 ,75 , 82 , 94 , 10, 11]
-
-        #Ha az index paros akkor belfoldi, ha paratlan akkor kulfoldi
-        repterek = {"Vienna" : "VIE" , "Budapest": "BUD", "Barcelona": "BCN", "Debrecen": "DEB", "Stockholm": "ARN", "Miskolc": "MCQ"}
-
-        print(x["name"])
-
-        repter_lista = list(repterek.values())
-        
-    #class belfoldi_jarat: 
-    class Belfoldi_jarat(Jarat):
-        def __init__(self, azonosito, repterek):
-            #super().__init__(fname, lname)  init(name, lname)
-            self.azonosito = azonosito
-            self.repterek = repterek
-
-    
-
-        belfoldi_azon = azonosito
-
-    #class nemzetkozi_jarat: 
-    class Nemzetkozi_jarat(Jarat):
-            # azonosito = 
-            # nemzetkozi_azonosito =
-
-    #class legi_tarsasag:
-    class Legi_tarsasag(Belfoldi_jarat, Nemzetkozi_jarat):
-            # azonosito = 
-            # jaratok = []
-
-    #class jegy_foglalas:
-    class Jegy_foglalas(Jarat):
-        def __init__(self, azonosito):
-            #super().__init__(fname, lname)  init(name, lname)
-            self.azonosito = azonosito 
-        
-
-        def jegy_foglalas(azonosito, ar):
-
-
-        def jegy_foglalas_lemondas(azonosito):
-
-
-        def jegy_foglalasok_listazasa(foglalas):
 
 
 ## Felhasználói interfész
@@ -117,5 +181,7 @@ class main_menu():
 
 if __name__ == "__main__":
     while True:
-        option = main_menu()
-        program(option)
+        print ("valassz opciot")
+        option = input()
+
+        main_menu.program(option)
