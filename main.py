60cd7e889dc884d5969095ca82f0378686ea59ab
z-zeek <167027652+z-zeek@users.noreply.github.com>
2025-05-25 14:35:01 +0200

    upload next version ig

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import os
 
-
-def clear_screen():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
 ## Fő osztályok
 
 ## Járat (absztrakt osztály): Definiálja a járat alapvető attribútumait (járatszám, célállomás, jegyár).
@@ -15,57 +8,111 @@ def clear_screen():
 ## LégiTársaság: Tartalmazza a járatokat és saját attribútumot, mint például a légitársaság neve.
 ## JegyFoglalás: A járatok foglalásához szükséges osztály, amely egy utazásra szóló jegy foglalását tárolja.
 
+class main_menu():
 
-class jarat:
+    def init_foglalas():
+        #does txt exist?
+        #if no:
+        #   - create and populate sample jaratok
+        #
+        #if yes:
+        #   - load it in
+
+    def program(option):
+        if option == '1':
+            ## Foglalasi funkcio
+            Jegyfoglalas.jegy_foglalas
+        elif option == '2':
+            ## Lemondasi funkcio
+        elif option == '3':
+            ## Listazasi funkcio 
+        elif option == '9':
+            print("A program bezárása...")
+            exit()
+        else:
+            print("Helytelen menüpont, fussunk neki megegyszer!")
+        input("\nNyomjon Entert a folytatáshoz...")
+
+    def clear_screen():
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
 
 
-class belfoldi_jarat:
+#Osztaly: jarat()
+#
+#vars: jaratszam 
+#- minden repter kap egy jaratszamot ami egy 3 jegyu szam,
+# maga a jarat a kindulo illetve a celallomas szamjegyeinek osszeadasa alkot egy jaratot
+# pl: Eger -> Vienna = 178250 (maybe 178-250), could also be startAirportLetters1725
+#
+#celallomas
+#- 
+#
+#jegyar
+#
+#
+
+    class Jarat():
+        azonositok = [17, 25, 31, 46, 59, 63 ,75 , 82 , 94 , 10, 11]
+
+        #Ha az index paros akkor belfoldi, ha paratlan akkor kulfoldi
+        repterek = {"Vienna" : "VIE" , "Budapest": "BUD", "Barcelona": "BCN", "Debrecen": "DEB", "Stockholm": "ARN", "Miskolc": "MCQ"}
+
+        print(x["name"])
+
+        repter_lista = list(repterek.values())
+        
+    #class belfoldi_jarat: 
+    class Belfoldi_jarat(Jarat):
+        def __init__(self, azonosito, repterek):
+            #super().__init__(fname, lname)  init(name, lname)
+            self.azonosito = azonosito
+            self.repterek = repterek
+
+    
+
+        belfoldi_azon = azonosito
+
+    #class nemzetkozi_jarat: 
+    class Nemzetkozi_jarat(Jarat):
+            # azonosito = 
+            # nemzetkozi_azonosito =
+
+    #class legi_tarsasag:
+    class Legi_tarsasag(Belfoldi_jarat, Nemzetkozi_jarat):
+            # azonosito = 
+            # jaratok = []
+
+    #class jegy_foglalas:
+    class Jegy_foglalas(Jarat):
+        def __init__(self, azonosito):
+            #super().__init__(fname, lname)  init(name, lname)
+            self.azonosito = azonosito 
+        
+
+        def jegy_foglalas(azonosito, ar):
 
 
-class nemzetkozi_jarat:
+        def jegy_foglalas_lemondas(azonosito):
 
 
-class legi_tarsasag:
-
-
-class jegy_foglalas:
+        def jegy_foglalasok_listazasa(foglalas):
 
 
 ## Felhasználói interfész
 
-## Egyszerű felhasználói interfész, amely lehetővé teszi a következő műveleteket:
+## Egyszerű felhasználói interfész, amely opciokent lehetővé teszi a következő műveleteket:
 ## Foglalás lemondása
 ## Jegy foglalása
 ## Foglalások listázása
-
-def program(option):
-    if option == '1':
-        ## Foglalasi funkcio
-    elif option == '2':
-        ## Lemondasi funkcio
-    elif option == '3':
-        ## Listazasi funkcio 
-    elif option == '9':
-        print("A program bezárása...")
-        exit()
-    else:
-        print("Helytelen menüpont, fussunk neki megegyszer!")
-    input("\nNyomjon Entert a folytatáshoz...")
-
 
 ## Funkciók
 
 ## Jegy foglalása: A járatokra jegyet lehet foglalni, és visszaadja a foglalás árát.
 ## Foglalás lemondása: A felhasználó lemondhatja a meglévő foglalást.
 ## Foglalások listázása: Az összes aktuális foglalás listázása.
-
-def jegy_foglalas():
-
-
-def jegy_foglalas_lemondas():
-
-
-def jegy_foglalasok_listazasa():
 
 
 if __name__ == "__main__":
